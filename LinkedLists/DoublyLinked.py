186130e315302e1e18f6abe0b25f8eb9ac7eb9cf
Kyle Bergman <60984164+bergmankyle99@users.noreply.github.com>
2026-08-23 14:56:07 -0700

    now managing head and tail of DLL

--- LinkedLists/DoublyLinked.py
+++ LinkedLists/DoublyLinked.py
@@ -14,98 +14,115 @@ class DoublyNode:
     def __str__(self):
         return str(self.val)
 
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
 
-Head = Tail = DoublyNode(1)
+    def display(self, head):
+        curr = head
+        elements = []
+        while curr:
+            elements.append(str(curr.val))
+            curr = curr.next
+        print("<->".join(elements))
 
-def display(head):
-    curr = head
-    elements = []
-    while curr:
-        elements.append(str(curr.val))
-        curr = curr.next
-    print("<->".join(elements))
+    # insert at beginning O(1)
+    def insert_at_beginning(self, head, val):
+        new_node = DoublyNode(val)
+        head.prev = new_node
+        new_node.next = head
+        return new_node
 
+    # insert at position O(n)
+    def insert_at_position(self, head, val, pos):
+        new_node = DoublyNode(val)
+        if pos == 0:
+            return self.insert_at_beginning(head, val)
+        curr = head
+        idx = 0
+        while curr:
+            if idx == pos:
+                nxt = curr
+                prev = curr.prev
+                prev.next = new_node
+                nxt.prev = new_node
+                new_node.prev = prev
+                new_node.next = nxt
+                return head
+            curr = curr.next
+            idx += 1
+        self.tail.next = new_node
+        new_node.prev = self.tail
+        self.tail = new_node
+        return head
+
+    # delete at beginning
+    def delete_at_beginning(self, head: DoublyNode):
+        curr = head.next
+        curr.prev = None
+        return curr
+
+    # delete node
+    def delete_at_pos(self, head, pos):
+        if pos == 0:
+            return self.delete_at_beginning(head)
+        curr = head
+        idx = 0
+        while curr:
+            if idx == pos:
+                prev = curr.prev
+                nxt = curr.next
+                prev.next = nxt
+                if nxt is not None:
+                    nxt.prev = prev
+                else:
+                    self.tail = prev
+                return head
+            curr = curr.next
+            idx += 1
+
+        if curr is None:
+            raise IndexError("Position out of bounds")
+        return curr
+
+    def search(self, head: DoublyNode, val):
+        curr = head
+        idx = 0
+        while curr:
+            if curr.val == val:
+                return idx
+            curr = curr.next
+            idx += 1
+        return None
+
+dll = DoublyLinkedList()
+dll.head = dll.tail = DoublyNode(1)
 print("Tail")
-display(Tail)
+dll.display(dll.tail)
 
-#insert at beginning O(1)
-def insert_at_beginning(head,  val):
-    new_node = DoublyNode(val)
-    head.prev = new_node
-    new_node.next = head
-    return new_node
-Head = insert_at_beginning(Head, 7)
+dll.head = dll.insert_at_beginning(dll.head, 7)
 print("Insert At Beginning")
-display(Head)
+dll.display(dll.head)
 
-#insert at position O(n)
-def insert_at_position(head, val, pos):
-    new_node = DoublyNode(val)
-    if pos == 0:
-        return insert_at_beginning(head, val)
-    curr = head
-    idx = 0
-    while curr:
-        if idx == pos:
-            nxt = curr
-            prev = curr.prev
-            prev.next = new_node
-            nxt.prev = new_node
-            new_node.prev = prev
-            new_node.next = nxt
-            return head
-        curr = curr.next
-        idx += 1
-    if curr is None:
-        raise IndexError("Position out of bounds")
-    return head
-
-Head = insert_at_position(Head, 9, 1)
+dll.head = dll.insert_at_position(dll.head, 9, 1)
 print("Insert at Pos")
-display(Head)
+dll.display(dll.head)
 
-#delete at beginning
-def delete_at_beginning(head):
-    curr = head.next
-    curr.prev = None
-    return curr
+dll.head = dll.insert_at_position(dll.head, 11, 3)
+print("Insert at Pos")
+dll.display(dll.head)
+print("Tail: "+str(dll.tail))
 
-#delete node
-def delete_at_pos(head, pos):
-    if pos == 0:
-        return delete_at_beginning(head)
-    curr = head
-    idx = 0
-    while curr:
-        if idx == pos:
-            prev = curr.prev
-            nxt = curr.next
-            prev.next = nxt
-            if nxt is not None:
-                nxt.prev = prev
-            return head
-        curr = curr.next
-        idx += 1
-    if curr is None:
-        raise IndexError("Position out of bounds")
-    return curr
 print("Delete at Pos")
-Head = delete_at_pos(Head, 2)
-display(Head)
+dll.head = dll.delete_at_pos(dll.head, 2)
+dll.display(dll.head)
 
-def search(head, val):
-    curr = head
-    idx = 0
-    while curr:
-        if curr.val == val:
-            return idx
-        curr = curr.next
-        idx += 1
-    return None
-
-res = search(Head, 8)
+res = dll.search(dll.head, 11)
 if res:
-    Head = delete_at_pos(Head, res)
-    display(Head)
+    dll.head = dll.delete_at_pos(dll.head, res)
+    dll.display(dll.head)
 else:
     print("Doesn't Exist")
+
+print("Tail: "+str(dll.tail))
